6a5c80a7b36208e6edafb77b8ebad93821a972b0
Наталья <123063048+Nataliy1997@users.noreply.github.com>
2023-02-12 12:11:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите функцию, которая принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.Нельзя использовать библиотеку Math!
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+void main()
+{
+    Console.Clear();
+    Console.WriteLine("Введи число А: ");
+    int A=int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введи число B: ");
+    int B=int.Parse(Console.ReadLine()!);
+    int pow=Stepen(A,B);
+    Console.Write(pow);
+}
+int Stepen(int A, int B)
+{
+    int result=1;
+    for (int count=1;count<=B;count++)
+    {
+        result=result*A;
+    }
+    return result;
+}
+main();
